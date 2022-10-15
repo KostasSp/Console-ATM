4fb5ae45e4a6efe3f23bcc6e771d213391cd0137
Kostas <kostasspanolios@gmail.com>
2022-10-15 15:58:15 +0100

    add constructor, setters and getters

--- Program.cs
+++ Program.cs
@@ -1,5 +1,64 @@
 ﻿using System;
 
-public class CardHolder {
-       
+public class CardHolder
+{
+       string cardNum;
+       int pin;
+       string firstName;
+       string lastName;
+       double balance;
+
+       public CardHolder(string cardNum, int pin, string firstName, string lastName, double balance)
+       {
+              this.cardNum = cardNum;
+              this.pin = pin;
+              this.firstName = firstName;
+              this.lastName = lastName;
+              this.balance = balance;
+       }
+
+       public string getNum()
+       {
+              return cardNum;
+       }
+
+       public string getFirstName()
+       {
+              return firstName;
+       }
+
+       public string getLastName()
+       {
+              return lastName;
+       }
+
+       public double getBalance()
+       {
+              return balance;
+       }
+
+       public void setNum(string num)
+       {
+              cardNum = num;
+       }
+
+       public void setPin(int newPin)
+       {
+              pin = newPin;
+       }
+
+       public void setFirstName(string fname)
+       {
+              firstName = fname;
+       }
+
+       public void setLastName(string lname)
+       {
+              lastName = lname;
+       }
+
+       public void setBalance(double newBalance)
+       {
+              balance = newBalance;
+       }
 }
