3b0a9353a4266d5c90e202703ac06c6a395bee69
Kostas <kostasspanolios@gmail.com>
2022-10-16 07:57:29 +0100

    add withdraw and view balance methods

--- Program.cs
+++ Program.cs
@@ -71,6 +71,13 @@ public class CardHolder
               Console.WriteLine("2. Withdraw");
               Console.WriteLine("3. Show Balance");
               Console.WriteLine("4. Exit");
+              // char choice = new char();
+              int choice = Int32.Parse(Console.ReadLine());
+              // byte[] choice = Byte.Parse();
+              if (choice != 4)
+              {
+                     Console.WriteLine("hey");
+              }
        }
 
        //passing the cardholder as param here is useless, as it is within scope - leave it for practice
@@ -80,13 +87,41 @@ public class CardHolder
               try
               {
                      double depAmount = Double.Parse(Console.ReadLine());
-                     currUser.setBalance(balance + depAmount);
+                     currUser.setBalance(currUser.getBalance() + depAmount);
                      Console.WriteLine("Transaction complete. New Balance: " + currUser.getBalance());
               }
               catch (Exception ex)
               {
                      Console.Write($"An error occurred -> {ex.Message}");
               }
+       }
+
+       void withdraw(CardHolder currUser)
+       {
+              Console.WriteLine("How much do you want to deposit?");
+              try
+              {
+                     double withdrawAmount = Double.Parse(Console.ReadLine());
+                     if (withdrawAmount < currUser.getBalance())
+                     {
+                            currUser.setBalance(currUser.getBalance() - withdrawAmount);
+                     }
+                     else
+                     {
+                            Console.WriteLine("You cannot withdraw more than your current balance");
+                     }
+
+                     Console.WriteLine("Transaction complete. New Balance: " + currUser.getBalance());
+              }
+              catch (Exception ex)
+              {
+                     Console.Write($"An error occurred -> {ex.Message}");
+              }
+       }
+
+       void getCurrentBalance(CardHolder currUser)
+       {
+              Console.WriteLine("Your current balance is: " + currUser.getBalance());
        }
 
        public static void Main(string[] args)
